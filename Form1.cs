069049110d8dfe43122d584c7701ad21c511fb87
Nico Felten <n_felten@hotmail.com>
2023-10-04 10:52:19 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormWithTestControls
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void chkMale_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkMale.Checked == true && chkFemale.Checked == true) {chkFemale.Checked = false;}
+        }
+
+        private void chkFemale_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkMale.Checked == true && chkFemale.Checked == true) {chkMale.Checked = false;}
+        }
+
+        private void txtInteger_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsDigit(e.KeyChar) || (e.KeyChar == (char)Keys.Back))) {e.Handled = true;}
+        }
+
+        private void txtFloat_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string st = "0123456789." + (char)8;
+            TextBox txtObj = sender as TextBox;
+            if (st.IndexOf(e.KeyChar) == -1)
+            {
+               e.Handled = true;
+            }
+            if (e.KeyChar == '.' && (txtObj.Text =="" || txtObj.Text.IndexOf('.') != -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtFloat_LostFocus(object sender, EventArgs e)
+        {
+            TextBox txtObj = sender as TextBox;
+            if (txtObj.Text.StartsWith(".")) { txtObj.Text = "0" + txtObj.Text;}
+            if (txtObj.Text.EndsWith(".")) { txtObj.Text = txtObj.Text.Replace(".","");}
+            string strTemp;
+           // strTemp = txtObj.Left(txtObj.Text.IndexOf(".")) //
+        }
+    }
+}
